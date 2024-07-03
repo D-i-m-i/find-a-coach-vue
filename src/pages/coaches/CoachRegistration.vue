82ac9099557975a563f26be0d5c583b38cdf8db9
Demetris Tzionis <demetristz@hotmail.com>
2024-07-03 13:58:32 +0300

    Commit initial files

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,22 @@
+<template>
+  <base-card>
+    <section><h2>Register as a coach now!</h2></section>
+    <coach-form @save-data="saveData"></coach-form>
+  </base-card>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches'); // 'replace' does not allow us to go back to where we came from. This is good here as we do not want to go back to the form (better user experience)
+    },
+  },
+};
+</script>
